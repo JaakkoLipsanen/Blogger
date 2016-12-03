32af2bf3ccaeeadb4024e46e54c21d0f61304750
JaakkoLipsanen <jaakko.lipsanen@outlook.com>
2016-12-03 19:44:05 +0200

    Removed some unnecessary stuff (top menu related)

--- app/src/main/java/flai/blogger/MainPage.java
+++ app/src/main/java/flai/blogger/MainPage.java
@@ -183,30 +183,6 @@ public class MainPage extends AppCompatActivity {
         _blogPostEndDayEditText.setText(Integer.toString(dayRange.EndDate));
     }
 
-    /* DUNNO WHAT THIS IS, AUTO GENERATED */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_page, menu);
-        return true;
-    }
-
-    /* DUNNO WHAT THIS IS, AUTO GENERATED */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     // display the clicked image entry in fullscreen popup
     private void setupOnImageEntryClicked() {
         final MainPage mainPage = this;
